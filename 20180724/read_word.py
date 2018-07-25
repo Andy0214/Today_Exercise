7b19cb273922df9d4011103803ce926415624ffc
Andy0214 <810432812@qq.com>
2018-07-25 09:57:49 +0800

    updata

--- 20180724/read_word.py
+++ 20180724/read_word.py
@@ -11,11 +11,17 @@ l = [paragraph.text.encode('gb2312') for paragraph in document.paragraphs]
 #输出并观察结果，也可以通过其他手段处理文本即可
 for i in l:
     print(i)
+
 #读取表格材料，并输出结果
 tables = [table for table in document.tables]
 for table in tables:
+    print("table", table)
+    hdr_cells = table.rows[0].cells  #表格的第一行元件对象
+    print(hdr_cells[0].text)     #第一行的第一个单元格文本
     for row in table.rows:
+        print("row", row)
         for cell in row.cells:
+            print("cell", cell)
             print(cell.text.encode('gb2312'), '\t',)
         print
     print('\n')
